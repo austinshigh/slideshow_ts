da240dd9fabe5d0a90fe861bd4232057ed499ad2
Austin <austinstewarthigh@gmail.com>
2022-02-16 17:19:05 -0500

    class based implementation

--- dist/clock.js
+++ dist/clock.js
@@ -1,20 +1,24 @@
-function startClock() {
+class Clock {
+  constructor(targetClock) {
+    this.clock = targetClock;
+  }
+  updateClock() {
     const date = new Date();
     let h = Number(date.getHours());
     let m = Number(date.getMinutes());
     let s = Number(date.getSeconds());
-    if (h > 12)
-        h = h - 12;
-    if (h < 10)
-        h = "0" + h;
-    if (m < 10)
-        m = "0" + m;
-    if (s < 10)
-        s = "0" + s;
+    if (h > 12) h = h - 12;
+    if (h < 10) h = "0" + h;
+    if (m < 10) m = "0" + m;
+    if (s < 10) s = "0" + s;
     let time = `${h}:${m}:${s}`;
-    document.getElementById("clock").innerHTML = time;
+    this.clock.innerHTML = time;
+  }
 }
-window.setInterval(function () {
-    startClock();
-}, 1000);
+document.addEventListener("DOMContentLoaded", () => {
+  const clock = new Clock(document.getElementById("clock"));
+  window.setInterval(function () {
+    clock.updateClock();
+  }, 1000);
+});
 //# sourceMappingURL=clock.js.map
--- src/clock.ts
+++ src/clock.ts
@@ -1,18 +1,30 @@
-function startClock() {
-  const date = new Date();
-  let h: string | number = Number(date.getHours());
-  let m: string | number = Number(date.getMinutes());
-  let s: string | number = Number(date.getSeconds());
+class Clock {
+  clock: HTMLElement;
 
-  if (h > 12) h = h - 12;
-  if (h < 10) h = "0" + h;
-  if (m < 10) m = "0" + m;
-  if (s < 10) s = "0" + s;
+  constructor(targetClock: HTMLElement | null) {
+    this.clock = targetClock;
+  }
 
-  let time: string = `${h}:${m}:${s}`;
-  document.getElementById("clock").innerHTML = time;
+  updateClock() {
+    const date = new Date();
+    let h: string | number = Number(date.getHours());
+    let m: string | number = Number(date.getMinutes());
+    let s: string | number = Number(date.getSeconds());
+
+    if (h > 12) h = h - 12;
+    if (h < 10) h = "0" + h;
+    if (m < 10) m = "0" + m;
+    if (s < 10) s = "0" + s;
+
+    let time: string = `${h}:${m}:${s}`;
+    this.clock.innerHTML = time;
+  }
 }
 
-window.setInterval(function () {
-  startClock();
-}, 1000);
+document.addEventListener("DOMContentLoaded", () => {
+  const clock: Clock = new Clock(document.getElementById("clock"));
+
+  window.setInterval(function () {
+    clock.updateClock();
+  }, 1000);
+});
